09e425159b6fecde43961b538f448bf2b92d5cec
PMWeill <pierre-manuel.weill@laposte.net>
2023-08-30 17:01:37 +0200

    add

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,16 +13,51 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View
+  View,
 } from 'react-native';
-import { articles } from './articles';
+import {articles} from './articles';
 import { MenuContext } from './MenuContext';
-import  {BoutiqueContext}  from './BoutiqueContext';
-import { NavMenu } from './components/NavMenu/NavMenu';
-import  { Boutique }  from './components/Boutique/Boutique';
+import { BoutiqueContext } from './BoutiqueContext';
+import { NavMenu } from './components/Menu/NavMenu';
+import {Boutique }from './components/Boutique/Boutique';
+// import { Footer } from './components/Footer/Footer';
 import { Panier } from './components/Panier/Panier';
 
 function App(): JSX.Element {
+
+
+  // declaration des mes states
+  const [stateMenu, setStateMenu] = useState(
+    {
+      "displayPanier": false,
+      "displayBoutique": true,
+      "displayContact": false,
+      "displayUl": false,
+      "tabMenuNav": [
+        {
+          text: "Magasin",
+          url: "#",
+          isActive: false
+        },
+        {
+          text: "Panier",
+          url: "#",
+          isActive: false
+        },
+        {
+          text: "Contact",
+          url: "#",
+          isActive: false
+        }
+      ],
+      "burgerButton": burgerButton,
+      "fonctDisplayPanier": fonctDisplayPanier,
+      "fonctDisplayBoutique": fonctDisplayBoutique,
+      "updateOrientation": false,
+
+      /* ajouter les deux fonction display pour Boutique et Contact */
+    }
+  )
   const [stateArticles, setStateArticles] = useState(
     {
       "articles": articles,
@@ -26,97 +67,106 @@ function App(): JSX.Element {
       "incrementQte": incrementQte
     }
   );
-  const [stateMenu, setStateMenu] = useState(
-    {
-      "displayPanier": false,
-      "displayUl": false,
-      "tabMenuNav": [
-        {
-          text: "Magasin",
-          url: "#",
-          isActive: false,
-        },
-        {
-          text: "Panier",
-          url: "#",
-          isActive: false,
-        },
-        {
-          text: "Contact",
-          url: "#",
-          isActive: false,
-        },
-      ],
-      "burgerButton": burgerButton,
-      "fonctDisplayPanier": fonctDisplayPanier
+  // detection mobile
+  function burgerButton(disp: Boolean) {
+    //let displayUlTmp = !stateMenu.displayUl
+    /* if (disp) {
 
+      document.body.style.height = 'auto';
+      document.body.style.overflowY = 'visible';
+
+    } else {
+      document.body.style.height = '100vh';
+      document.body.style.overflowY = 'hidden';
     }
-  )
-  
-  function burgerButton(disp: boolean) {
     setStateMenu({
       ...stateMenu,
       "displayUl": !disp
-    })
-
+    }) */
   }
-  function fonctDisplayPanier(disp: boolean) {
-    // if (disp) {
-    //   document.body.style.height = "auto";
-    //   document.body.style.overflow = "visible";
-    // } else {
-    //   document.body.style.height = "100vh";
-    //   document.body.style.overflow = "hidden";
-    // }
+
+  function fonctDisplayPanier(disp: Boolean) {
+    /* if (disp) {
+      document.body.style.height = 'auto';
+      document.body.style.overflowY = 'visible';
+
+    } else {
+      document.body.style.height = '100vh';
+      document.body.style.overflowY = 'hidden';
+    }
     setStateMenu({
       ...stateMenu,
+      "displayUl": disp,
       "displayPanier": !disp
-    })
+    }) */
 
   }
-  function decrementQte(id: number) {
+  /* creer une fonction pour l'affichage de boutique en fonction de displayBoutique ... */
+  /* creer une fonction pour l'affichage de boutique en fonction de displayContact ... */
+  function fonctDisplayBoutique() {
+    setStateMenu({
+      ...stateMenu,
+      "displayBoutique": !stateMenu.displayBoutique,
+      "displayContact": !stateMenu.displayContact,
+    })
+  }
+  function decrementQte(id: Number) {
+    console.log(id);
+    // je fais une copie de mon tableau stateArticles car il est en lecture seule
+    // et je ne peux pas le modfifier directement.
     let articlesTmp = stateArticles.articles;
+    // je modifie la qte de l'article correspondant à l'id transmis par mon component Bouton
     articlesTmp.map((valeur, index) => {
-
       if (index === id) {
         valeur.qte > 0 && --valeur.qte;
-
+        //valeur.qte === 0 ? valeur.qte = 0 : valeur.qte -= 1;
+        // if (valeur.qte === 0) {
+        //   valeur.qte = 0;
+        // } else {
+        //   valeur.qte -= 1;
+        // }
       }
     })
-
+    // j'ajoute l'id de l'article acheté au tableau stateArticles.tabPanier
     const tmpTabPanier = stateArticles.tabPanier;
     tmpTabPanier.push(id)
-    setStateArticles({
-      ...stateArticles,
-      "articles": articlesTmp,
-      "tabPanier": tmpTabPanier
-    });
+    console.dir(tmpTabPanier);
+    // je réassigne le nouveau tableau article modifié à mon stateArticles
+    // grace à sa fonction setStateArticles
+    setStateArticles(
+      {
+        ...stateArticles,// le ...objet, rappelle toutes les propriétées:valeur de l'objet
+        "articles": articlesTmp,
+        "tabPanier": tmpTabPanier
+      }
+    );
+    //mise a jour du total du panier dans le state
     calculTotal()
   }
-
-
-  function incrementQte(id: number) {
+  function incrementQte(id: Number) {
+    // je crée une variable pour l'index du tableau tabPanier
+    // que je vais supprimer
     let supprIndex: Number;
     stateArticles.tabPanier.find((value, index) => {
       if (value === id) {
-
+        // je récupere dans mon tableau stateArticles.tabPanier l'index de
+        // l'article à supprimer
+        supprIndex = index;
       }
     })
-
+    // array.splice(index,nb_elements) me permet de supprimer l'index supprIndex
     const tmpTabPanier = stateArticles.tabPanier;
     tmpTabPanier.splice(supprIndex, 1);
-
+    // Comme dans la fonction précédente je doit maintenant gérer la quantité 
+    // d'article disponible et y ajouter une unité
     let articlesTmp = stateArticles.articles;
     articlesTmp.map((valeur, index) => {
-
       if (index === id) {
-        valeur.qte > 0 && ++valeur.qte;
-
+        ++valeur.qte;
       }
     })
-
-
-
+    // une fois toutes les modifications effectuées sur mes tableaux temporaires
+    // je peux les affecter à mes state (set)
     setStateArticles(
       {
         ...stateArticles,
@@ -124,12 +174,13 @@ function App(): JSX.Element {
         "tabPanier": tmpTabPanier
       }
     );
-    calculTotal()
+    //mise a jour du total du panier dans le state
+    calculTotal();
   }
   function calculTotal() {
     let totalTmp = 0;
-    stateArticles.tabPanier.map((valeur,) => {
-      totalTmp += stateArticles.articles[valeur].price
+    stateArticles.tabPanier.map((valeur) => {
+      totalTmp += stateArticles.articles[valeur].price;
     })
     setStateArticles(
       {
@@ -139,27 +190,22 @@ function App(): JSX.Element {
     )
   }
 
-
-
   return (
     <MenuContext.Provider value={stateMenu}>
       <BoutiqueContext.Provider value={stateArticles}>
-        <SafeAreaView >
+        <SafeAreaView>
           <StatusBar />
-
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
-            style={styles.bgScrollView}
+            style={styles.background}
           >
-            <View style={styles.bgScrollView}>
-            <NavMenu></NavMenu>
-             <Panier></Panier>
-            <Boutique articles={stateArticles.articles}></Boutique>
-              
-             
-            </View >
+            <View>
+              <NavMenu></NavMenu>
+              <Boutique articles={stateArticles.articles}></Boutique>
+              <Panier></Panier>
+              {/* <Footer></Footer> */}
+            </View>
           </ScrollView>
-
         </SafeAreaView>
       </BoutiqueContext.Provider>
     </MenuContext.Provider>
@@ -167,14 +213,10 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  bgScrollView: {
+  background: {
+    backgroundColor: "#bdd6fc",
     height: "auto",
-    backgroundColor: "yellow"
   },
-  monMenu: {
-    backgroundColor: "green",
-    height: 250,
-  }
 });
 
-export { App };
+export default App;
--- components/Panier/Panier.jsx
+++ components/Panier/Panier.jsx
@@ -1,84 +1,145 @@
-import React,{ useContext } from "react";
-
+import { useContext } from "react";
+import { View, Text, Image, StyleSheet } from "react-native"
+import { Button } from "react-native-paper"
 import { BoutiqueContext } from "../../BoutiqueContext";
 import { MenuContext } from "../../MenuContext";
-import {Button} from "react-native-paper";
-import { Text, View, Image  } from "react-native";
 const Panier = () => {
     const boutiqueContext = useContext(BoutiqueContext);
     const menuContext = useContext(MenuContext);
     const paniertmp = [];
     if (boutiqueContext.tabPanier.length > 0) {
-        // sort permet de trier mon panier
         const tabPanier = boutiqueContext.tabPanier.sort();
-
         let i = 0;
         tabPanier.map((valeur, index) => {
             i++;
             if (valeur !== tabPanier[index + 1]) {
                 paniertmp.push([valeur, i]);
-                i = 0
+                i = 0;
             }
-
         })
-      
     }
-
     return (
-        <View>
-                <Text>Votre panier</Text>
+        <View style={styles.backPanier}>
+            <View style={styles.panier}>
+                <View style={styles.innerPanier}>
+                    
+                    <Text style={styles.h2}>Votre panier</Text>
+                    <View style={styles.art}>
+                        {
+                            /* je boucle sur le tableau tabPanier qui contient les id de mes
+                            articles achetés */
+                            boutiqueContext.tabPanier.length > 0 ?
 
-
-                <View>
-
+                                paniertmp.map((valeur, index) => {
+                                    let name = boutiqueContext.articles[valeur[0]].name;
+                                    let priceu = boutiqueContext.articles[valeur[0]].price;
+                                    let pricet = boutiqueContext.articles[valeur[0]].price * valeur[1];
+                                    let qtea = valeur[1];
+                                    let url = boutiqueContext.articles[valeur[0]].url;
+                                    // sur le modele de mon Bouton.js je "surveille" la qte disponible
+                                    // pour rendre mon bouton + actif ou inactif
+                                    let isActiveplus = boutiqueContext.articles[valeur[0]].qte === 0 ? true : false
+                                    return (
+                                        <View
+                                            style={styles.panierEntries}
+                                            key={index}>
+                                            <Image source={url} style={styles.panImg}/>
+                                            <Text style={styles.panName}>{name}</Text>
+                                            <Text>{styles.priceu}€/unité</Text>
+                                            <Text>
+                                                <Button style={styles.panbtn}
+                                                    disabled={isActiveplus}
+                                                    onPress={() => {
+                                                        /* depuis le onClick sur ce bouton j'appelle la fonction decrementQte
+                                                        liée à mon stateArticles (et donc mon BoutiqueContext) dans App.js */
+                                                        boutiqueContext.decrementQte(valeur[0])
+                                                    }}
+                                                >+</Button>
+                                                <Text>{qtea}</Text>
+                                                <Button style={styles.panbtn}
+                                                    onPress={() => {
+                                                        boutiqueContext.incrementQte(valeur[0])
+                                                    }}
+                                                >-</Button>
+                                            </Text>
+                                            <Text style={styles.pricet}>{pricet}€</Text>
+                                        </View>
+                                    )
+                                })
+                                :
+                                <Text>Votre panier est actuellement vide.</Text>
+                        }
+                    </View>
                     {
                         boutiqueContext.tabPanier.length > 0 ?
-
-                            paniertmp.map((valeur, index) => {
-                                let name = boutiqueContext.articles[valeur[0]].name;
-                                let priceU = boutiqueContext.articles[valeur[0]].price
-                                let priceT = boutiqueContext.articles[valeur[0]].price * valeur[1];
-                                let qteA = valeur[1]
-                                let url = boutiqueContext.articles[valeur[0]].url
-                                let isActiveplus = boutiqueContext.articles[valeur[0]].qte === 0 ? true : false
-
-                                return (
-                                    <View key={index} >
-                                        <Image source={url} ></Image>
-                                        <View >
-                                            <Text>{name}</Text>
-                                            <Text >{priceU}€/unité</Text>
-                                        </View >
-                                        <View>
-                                            <Button
-                                                disabled={isActiveplus}
-                                                onPress={() => { boutiqueContext.decrementQte(valeur[0]) }} >+</Button>
-                                            <Text>{qteA}</Text>
-                                            <Button
-                                                onPress={() => {
-                                                    boutiqueContext.incrementQte(valeur[0])
-                                                }}
-                                            >-</Button>
-                                            <Text >{priceT}€</Text>
-                                        </View>
-
-                                    </View>
-                                )
-                            })
+                            <Text style={styles.totalPrice}>Votre total : {boutiqueContext.totalPanier} €</Text>
                             :
-                            <Text>Votre panier est actuellement vide</Text>
+                            <></>
                     }
-
-
                 </View>
-                {
-                    boutiqueContext.tabPanier.length > 0 ?
-                        <Text> Votre totale : {boutiqueContext.totalPanier} €</Text>
-                        :
-                        <></>
-                }
-           
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    backPanier: {
+        "backgroundColor": "rgba(0, 0, 0, .4)",
+        "height": "auto",
+        "display": "flex",
+    },
+    panier: {
+        "width": "80%",
+        "backgroundColor": "rgb(255, 255, 255)",
+        "padding": 16,
+        "color": "#464646",
+        "borderRadius": 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+    panierEntries: {
+        "width": "80%",
+        "margin": "0 auto",
+        "display": "flex",
+        "alignItems": "center",
+        "justifyContent": "space-between",
+        "borderBottom": "1px red solid",
+        "paddingBottom": 8,
+        "marginBottom": 8
+    },
+    panName: {
+        "fontWeight": "900",
+        "color": "red",
+        "width": 165,
+    },
+    panImg: {
+        "width": 80
+    },
+    panbtn: {
+        "padding": ".1rem .5rem .1rem .5rem",
+        "margin": 8,
+        "width": 25,
+        "fontWeight": "900",
+        "border": "1px gray solid",
+        "WebkitBoxShadow": "2px 2px 6px -1px rgba(0, 0, 0, 0.45)",
+        "boxShadow": "2px 2px 6px -1px rgba(0, 0, 0, 0.45)",
+        "borderRadius": 8,
+    },
+
+    totalPrice: {
+        "fontWeight": "900",
+        "textAlign": "right",
+        "width": "80%",
+        "margin": "0 auto"
+    },
+    pricet: {
+        "width": "45px",
+    }
+})
 export { Panier }
